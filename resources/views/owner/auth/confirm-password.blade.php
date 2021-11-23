54f9912aaf1227cac1465c03b7595d407255a948
yugo uchiida <y.uchiida@nirneege.com>
2021-11-24 01:11:05 +0900

    implement Owners CRUD(WIP)

--- resources/views/owner/auth/confirm-password.blade.php
+++ resources/views/owner/auth/confirm-password.blade.php
@@ -2,7 +2,9 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <div class="w-32">
+                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                </div>
             </a>
         </x-slot>
 
