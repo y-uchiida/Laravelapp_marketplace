323055353834b73da257f05fbba91a6200d51968
yugo uchiida <y.uchiida@nirneege.com>
2022-01-04 20:24:41 +0900

    add_swiper_slider in product detail page

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+ mix.js('resources/js/app.js', 'public/js') /* app.js を読み込み */
+ .js('resources/js/swiper.js', 'public/js') /* コンパイル対象に swiper.js を追加 */
+ .postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
