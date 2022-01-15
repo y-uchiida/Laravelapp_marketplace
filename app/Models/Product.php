6f23da51b03b768317e420b24c3e84f5bbd9ab48
yugo uchiida <y.uchiida@nirneege.com>
2022-01-15 22:34:21 +0900

    refactor ItemCotnroller, devide from querybuilder logic in index action to localscope of Item model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 /* リレーションさせるモデルを読み込み */
 use App\Models\Shop;
@@ -72,5 +73,48 @@ class Product extends Model
     {
         return $this->belongsToMany(User::class, 'carts')
         ->withPivot(['id', 'quantity']);
+    }
+
+    /*  */
+    public function scopeAvailableItems($query)
+    {
+
+        /* products の現在の在庫数を取得するクエリ
+         * stocks テーブルのquantityの値を、product_idで合計すると、
+         * 各商品の現在在庫がわかる
+         * サブクエリとして実行するので、get() を付けずにクエリビルダの状態で保持しておく
+         */
+        $stocks = DB::table('t_stocks')
+            ->select('product_id',
+                DB::raw('sum(quantity) as quantity'))
+            ->groupBy('product_id')
+            ->having('quantity', '>', 1);
+
+        /*
+         * 販売可能な商品の一覧を取得するクエリ
+         * Eloquent を使っていないので、そのままでは別テーブルの情報が結果に含まれない
+         * join() メソッドを使って、必要なカラムを持つテーブルと連結する
+         * joinSub() メソッドは、渡されたクエリを実行した結果をjoinするメソッド
+         * name などは多くのテーブルで利用されているカラム名で、重複があるので、
+         * どのテーブル化を特定できるように[テーブル名].[カラム名] の形で記述する
+         * また、images は、それぞれimage1~image4 のカラム名を付けている
+         */
+        return $query
+            ->joinSub($stocks, 'stock', function ($join) {
+                $join->on('products.id', '=', 'stock.product_id');
+            })
+            ->join('shops', 'products.shop_id', '=', 'shops.id')
+            ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
+            ->join('images as image1', 'products.image1', '=', 'image1.id')
+            ->join('images as image2', 'products.image2', '=', 'image2.id')
+            ->join('images as image3', 'products.image3', '=', 'image3.id')
+            ->join('images as image4', 'products.image4', '=', 'image4.id')
+            ->where('shops.is_selling', true)
+            ->where('products.is_selling', true)
+            ->select(
+                'products.id as id', 'products.name as name', 'products.price',
+                'products.sort_order as sort_order', 'products.information',
+                'secondary_categories.name as category', 'image1.filename as filename'
+            );
     }
 }
