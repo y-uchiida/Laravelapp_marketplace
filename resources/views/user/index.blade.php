eec172570bd92b947cafaf2e899d3ee7d8b0e3c3
yugo uchiida <y.uchiida@nirneege.com>
2022-01-23 06:33:09 +0900

    implement product sort

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,8 +1,32 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            ホーム
-        </h2>
+        <div class="flex justify-between items-center">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                商品一覧
+            </h2>
+            <div>
+                <form method="get" action="{{ route('user.items.index') }}">
+                    <div class="flex">
+                        <div>
+                            <span class="text-sm">表示順</span><br>
+                            <select id="sort" name="sort" class="mr-4">
+                                @foreach(\Constant::SORT_ORDER as $index => $order_type)
+                                    <option
+                                        value="{{ $order_type['value'] }}"
+                                        @if(\Request::get('sort') === $order_type['value'])
+                                            selected
+                                        @endif
+                                    >
+                                        {{ $order_type['label'] }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div>表示件数</div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </x-slot>
 
     <div class="py-12">
@@ -14,8 +38,7 @@
                             <div class="w-1/4 p-2 md:p-4">
                                 <a href="{{ route('user.items.show', ['item' => $product->id]) }}">
                                     <div class="border rounded-md p-2 md:p-4">
-                                        <x-thumbnail filename="{{ $product->filename ?? '' }}"
-                                            type="products" />
+                                        <x-thumbnail filename="{{ $product->filename ?? '' }}" type="products" />
 
                                         <div class="mt-4">
                                             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
@@ -35,4 +58,10 @@
             </div>
         </div>
     </div>
+    <script>
+        const select = document.getElementById('sort')
+        select.addEventListener('change', function(){
+            this.form.submit()
+        })
+    </script>
 </x-app-layout>
